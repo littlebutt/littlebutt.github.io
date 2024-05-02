5fc5f84b6b31373972575c881bf95c6e336760f3
littlebutt <luogan199686@gmail.com>
2024-05-02 14:38:33 +0800

    feat: change default images

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: '收录',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/planet.svg').default,
     description: (
       <>
         站点收录了自计算机发明以来富有历史意义的“古董”软件，包括操作系统、编译器、文本编辑器和专业软件等。
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: '体验',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('@site/static/img/computer.svg').default,
     description: (
       <>
         被收录软件会以可执行文件的方式保存在本站点以供访问者体验。如果无法提供某些软件的可运行环境，站点会提供源码以供参考。
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: '资料',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('@site/static/img/books.svg').default,
     description: (
       <>
         本站点会提供被收录软件的相关资料，包括操作手册、图片和设计文档等。
